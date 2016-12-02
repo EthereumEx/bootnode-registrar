568b3aab83c65fa1e17b0ca2cbc14c81f386b076
Eric Maino <ericmai@microsoft.com>
2016-12-02 07:06:45 +0100

    Updating to work with Parity

--- client.js
+++ client.js
@@ -4,23 +4,27 @@ var fs = require('fs');
 var request = require('request');
 var sleep = require('sleep');
 
+var ipcPath = "/home/geth/.geth/geth.ipc";
+
+if (process.env.ETH_IPC_PATH) {
+    ipcPath = process.env.ETH_IPC_PATH;
+}
+
 function web3Client() {
     this.failCount = 0;
 }
 
 web3Client.prototype.Refresh = function () {
-    if (this.failCount > 15)
-    {
+    if (this.failCount > 15) {
         throw new Error("Too many exceptions. . . Exiting")
     }
 
-    if (!this._web3)
-    {
+    if (!this._web3) {
         var client = net.Socket();
-        var web3 = new Web3(new Web3.providers.IpcProvider("/home/geth/.geth/geth.ipc", client));
+        var web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, client));
 
         web3._extend({
-            property: 'admin',
+            property: 'geth',
             properties:
             [
                 new web3._extend.Property({
@@ -30,8 +34,36 @@ web3Client.prototype.Refresh = function () {
             ]
         });
 
+        web3._extend({
+            property: 'parity',
+            properties:
+            [
+                new web3._extend.Property({
+                    name: 'nodeInfo',
+                    getter: 'parity_enode',
+                    outputFormatter: function (result) {
+                        var pattern = /enode\:\/\/([^@]+)@[^:]+:(.+)/g;
+                        var match = pattern.exec(result);
+
+                        if (match) {
+                            result = {
+                                id: match[1],
+                                ports: {
+                                    listener: match[2]
+                                }
+                            }
+                        }
+
+                        return result;
+                    }
+                }),
+            ]
+        });
+
         this._web3 = web3
-        this.admin = web3.admin;
+        this.default = web3.geth;
+        this.geth = web3.geth;
+        this.parity = web3.parity;
     }
 
     this._web3.reset();
@@ -41,15 +73,14 @@ function updateEnode(url, data, callback) {
     console.log("update enode - " + url);
     request.post(
         url,
-        {  
-            json : data,
-            timeout : 1000 
+        {
+            json: data,
+            timeout: 1000
         },
         function (error, response, body) {
             var e = error || response.statusCode != 200;
 
-            if (e)
-            {
+            if (e) {
                 console.log(error);
             }
             callback(e, response);
@@ -57,25 +88,53 @@ function updateEnode(url, data, callback) {
     )
 };
 
-function enodeUpdater(web3Client)
-{
+function enodeUpdater(web3Client) {
     this.web3 = web3Client;
 }
 
-function runLoop(obj, timeout)
-{
+function runLoop(obj, timeout) {
     setTimeout(function () {
         obj.Run(obj);
     }, timeout);
 }
 
+function readNode(web3, fn) {
+    web3.Refresh();
+
+    web3.default.getNodeInfo(function (error, result) {
+        if (error) {
+            web3.geth.getNodeInfo(function (error, result) {
+                if (error) {
+                    web3.parity.getNodeInfo(function (error, result) {
+
+                        if (!error)
+                        {
+                            web3.default = web3.parity;
+                        }
+
+                        fn(error, result);
+                    });
+                }
+                else
+                {
+                    web3.default = web3.geth;
+                    fn(error, result);
+                }
+            })
+        }
+        else
+        {
+            fn(error, result);
+        }
+    });
+}
+
 enodeUpdater.prototype.Run = function (obj) {
     var web3 = obj.web3;
-    web3.Refresh();
-    web3.admin.getNodeInfo(function (error, result) {
+    readNode(obj.web3, function (error, result) {
         var timeout = 1000 * 10;
         if (error) {
-            web3.failCount ++;
+            web3.failCount++;
             console.log("Fail count: " + web3.failCount + " " + error);
             runLoop(obj, 500);
         }
@@ -84,17 +143,15 @@ enodeUpdater.prototype.Run = function (obj) {
                 enode: result.id,
                 port: result.ports.listener,
                 ip: process.env.HOST_IP,
-                publicIp : process.env.BOOTNODE_PUBLIC_IP,
-                network : process.env.BOOTNODE_NETWORK,
-                miner : false || process.env.ENABLE_MINER
+                publicIp: process.env.BOOTNODE_PUBLIC_IP,
+                network: process.env.BOOTNODE_NETWORK,
+                miner: false || process.env.ENABLE_MINER
             }
-            updateEnode(process.env.BOOTNODE_URL, data, function(err, result){
-                if (err)
-                {
+            updateEnode(process.env.BOOTNODE_URL, data, function (err, result) {
+                if (err) {
                     runLoop(obj, 1000 * 3);
                 }
-                else
-                {
+                else {
                     console.log(data);
                     runLoop(obj, 1000 * 15);
                 }
@@ -104,13 +161,11 @@ enodeUpdater.prototype.Run = function (obj) {
 };
 
 
-if (process.env.BOOTNODE_URL)
-{
+if (process.env.BOOTNODE_URL) {
     var client = new web3Client();
     var enode = new enodeUpdater(client);
     enode.Run(enode);
 }
-else
-{
+else {
     console.log("No BOOTNODE_URL,BOOTNODE_NETWORK or BOOTNODE_PUBLIC_IP provided");
 }
